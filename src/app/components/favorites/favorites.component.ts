a179559b09b689b2c39eda395112ccbd5cf8b396
Charles Santos <csbs.conta@outlook.com>
2022-08-17 09:08:34 -0300

    [favorites] layout

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FavoriteService } from '@app/shared/_index';
+import { FavoriteService, FavoritesInterface } from '@app/shared/_index';
 
 @Component({
   selector: 'ftc-favorites',
@@ -7,9 +7,32 @@ import { FavoriteService } from '@app/shared/_index';
   styleUrls: ['./favorites.component.scss'],
 })
 export class FavoritesComponent implements OnInit {
-  public favorites!: string[];
+  public favorites!: FavoritesInterface[];
 
-  constructor(private favoriteService: FavoriteService) {}
+  constructor(private favoriteService: FavoriteService) {
+    this.favorites = [
+      {
+        name: 'atendimento',
+        url: '/atendimento/atendimento',
+      },
+      {
+        name: 'priorização',
+        url: '/atendimento/priorizacao',
+      },
+      {
+        name: 'bloqueio de correspond./cobrança',
+        url: '/cliente/bloqueio-cobranca',
+      },
+      {
+        name: 'devices virtuais',
+        url: '/dispositivo/devices-virtuais',
+      },
+      {
+        name: 'faturas',
+        url: 'string',
+      },
+    ];
+  }
 
   ngOnInit(): void {
     // this.favorites = this.favoriteService.getAllFavorites();
