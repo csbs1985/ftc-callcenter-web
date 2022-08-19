ca3bd89ffdc748c35beef445346cf65953c99afa
Charles Santos <csbs.conta@outlook.com>
2022-08-19 08:17:30 -0300

    [identify-user] adiconar margem de baixo

--- src/app/libs/identify-user/identify-user.component.ts
+++ src/app/libs/identify-user/identify-user.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class IdentifyUserComponent implements OnInit {
   public userData: string = '';
 
+  public cpf = '[0-9]{3}.?[0-9]{3}.?[0-9]{3}-?[0-9]{2}';
+  public cnpj = '[0-9]{2}.?[0-9]{3}.?[0-9]{3}/?[0-9]{4}-?[0-9]{2}';
+  public code = '';
+
   constructor() {}
 
   ngOnInit(): void {}
